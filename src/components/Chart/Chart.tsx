cfe4016ddd4e6e8f3dfc2fb162a778751d63859e
dahhnym <danimkim.dev@gmail.com>
2022-11-29 19:59:29 +0900

    🚧 get sum of monthly amount for chart

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -2,13 +2,18 @@ import { FunctionComponent } from "react";
 import "./Chart.scss";
 import ChartBar from "./ChartBar";
 
-type ChartProp = {
-  selectedYear: string;
-};
+interface Props {
+  dataPoints: Array<{
+    label: string;
+    value: number;
+  }>;
+}
 
-const Chart: FunctionComponent<ChartProp> = (selectedYear) => {
+const Chart: FunctionComponent<Props> = ({ dataPoints }) => {
+  // ChartBar에 넘겨줄 데이터 : key(월), maxValue, value, label(월)
+
   return (
-    <div>
+    <div className='chart'>
       <ChartBar />
     </div>
   );
